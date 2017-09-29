198d8a111221b85d89d90212366873376ce00498
ajthyng <andrew.thyng@gmail.com>
2017-09-29 15:18:15 -0500

    Added a logout button for my own convenience, also for user convenience.

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 import {connect} from 'react-redux';
+import {logout} from '../../actions/login';
 
 class Settings extends Component {
 	constructor(props) {
 		super(props);
 	}
 
+	componentWillReceiveProps(newProps) {
+		if (!newProps.isLoggedIn) {
+			Actions.login();
+		}
+	}
+
 	render() {
 		return (
 			<View>
 				<Text>Hello from {this.props.title}!</Text>
+				<View style={{margin: 32}}>
+					<Button title="Logout"
+									onPress={() => this.props.userLogout()}/>
+				</View>
 			</View>
 		);
 	}
@@ -18,15 +30,13 @@ class Settings extends Component {
 
 const mapStateToProps = (state, ownProps) => {
 	return {
-		//Object mapping state variables to props variables
-		//netid: state.register.registeredUser
+		isLoggedIn: state.login.isLoggedIn
 	}
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		//Object mapping action functions to props as functions
-		//getMemberships: () => dispatch(getSiteInfo())        
+		userLogout: () => dispatch(logout())
 	}
 };
 
