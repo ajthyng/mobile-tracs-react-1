65497f1dec60298f5d8887a7dba55479a71b1166
ajthyng <andrew.thyng@gmail.com>
2017-10-12 12:50:57 -0500

    Added tests for location helper function

--- tests/utils/location.js
+++ tests/utils/location.js
@@ -7,3 +7,66 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+import * as location from '../../src/utils/location';
+
+it('should return false if currentScene is empty', () => {
+	const currentScene = '';
+	const reduxScene = 'login';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return false if reduxScene is empty', () => {
+	const currentScene = 'login';
+	const reduxScene = '';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return true if both scenes are equal', () => {
+	const currentScene = 'login';
+	const reduxScene = 'login';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(true);
+});
+
+it('should return false if both scenes are empty', () => {
+	const currentScene = '';
+	const reduxScene = '';
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return false if currentScene is undefined', () => {
+	const currentScene = undefined;
+	const reduxScene = 'login';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return false if reduxScene is undefined', () => {
+	const currentScene = 'login';
+	const reduxScene = undefined;
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return false if both scenes are undefined', () => {
+	const currentScene = undefined;
+	const reduxScene = undefined;
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it('should return false if scenes are different', () => {
+	const currentScene = 'login';
+	const reduxScene = 'sites';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
+
+it ('should return false if scenes are different types', () => {
+	const currentScene = 45;
+	const reduxScene = 'sites';
+
+	expect(location.compare(currentScene, reduxScene)).toEqual(false);
+});
