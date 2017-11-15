758d036a857ea3dcf061b83dc3e5e8945343aef5
ajthyng <andrew.thyng@gmail.com>
2017-11-15 08:42:03 -0600

    Changed the activity indicator to the tracs icon, someday it will rotate

--- src/components/Helper/ActivityIndicator.js
+++ src/components/Helper/ActivityIndicator.js
@@ -1,26 +1,32 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Image, Platform, StyleSheet, Text, View} from 'react-native';
 
 class ActivityIndicator extends Component {
 	constructor(props) {
 		super(props);
+		this.getStyle = this.getStyle.bind(this);
 	}
 
-	static getStyle() {
+	getStyle() {
 		return StyleSheet.create({
 			container: {
 				flex: 1,
 				flexDirection: 'row',
 				alignItems: 'center',
 				justifyContent: 'center'
+			},
+			image: {
+				width: 60,
+				height: 60
 			}
 		});
 	}
 
 	render() {
 		return (
-			<View style={ActivityIndicator.getStyle().container}>
-				<Text>Hello, {Platform.OS} is loading...</Text>
+			<View style={this.getStyle().container}>
+				<Image source={require('../../../img/tracs.png')}
+							 style={this.getStyle().image} />
 			</View>
 		)
 	}
