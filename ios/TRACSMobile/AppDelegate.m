31f3a0a2e6e4f275a1795607273149634835078a
ajthyng <andrew.thyng@gmail.com>
2018-04-03 10:54:17 -0500

    Updated versions for Test Flight and jsbundle location for a signed release

--- ios/TRACSMobile/AppDelegate.m
+++ ios/TRACSMobile/AppDelegate.m
@@ -27,8 +27,8 @@
 #endif
 
 //  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  jsCodeLocation = [NSURL URLWithString:@"http://147.26.119.60:8081/index.ios.bundle?platform=ios&dev=true"];
-//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://147.26.119.60:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"TRACSMobile"
                                                initialProperties:@{@"isSimulator": @(isSimulator)}
