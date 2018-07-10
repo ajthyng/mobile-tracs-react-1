86e774c26b9e3878649cb85416a21439e03569ce
ajthyng <andrew.thyng@gmail.com>
2018-07-10 12:16:14 -0500

    Remove unused imports and LogoutButton HOC

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import Ripple from 'react-native-material-ripple';
 import {View, StyleSheet, Animated} from 'react-native';
 import {logout} from '../../actions/login';
 import * as Storage from '../../utils/storage';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
 import CourseList from '../CourseList/CourseList';
 import Header from '../CircleHeader/Header';
 import {setHeaderState} from '../../actions/header';
@@ -17,47 +15,6 @@ const Home = styled.View`
 		background-color: ${props => props.theme.viewBackground};
 		width: 100%;
 `
-
-const LogoutButtonHOC = Comp => ({newRef, children, ...props}) => (
-	<Comp
-		style={{
-			flexDirection: 'row',
-			alignContent: 'center',
-			justifyContent: 'center',
-			backgroundColor: props.backgroundColor || "#1b3c80",
-			width: 70,
-			height: 70,
-			borderRadius: 35,
-			position: 'absolute',
-			right: 20,
-			bottom: 20,
-			elevation: 4,
-			shadowColor: '#000',
-			shadowOffset: {
-				width: 0,
-				height: 2
-			},
-			shadowRadius: 3,
-			shadowOpacity: 0.7
-		}}
-		ref={newRef}
-		{...props}
-	>
-		<Icon
-			style={{
-				color: "#fff",
-				fontSize: 25,
-				textAlign: 'center',
-				alignSelf: 'center',
-				marginRight: 7,
-				marginTop: 2
-			}}
-			name="logout"
-		/>
-	</Comp>
-);
-
-const LogoutButton = LogoutButtonHOC(Ripple);
 
 class HomeScreen extends Component {
 	static navigationOptions = {
