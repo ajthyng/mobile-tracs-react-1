9b063acd7c5ddf780da543af1f1638ca33076faa
ajthyng <andrew.thyng@gmail.com>
2018-07-10 16:51:21 -0500

    Remove redux logger, no longer needed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -10,13 +10,8 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
-
-const logger = createLogger({
-	collapsed: true,
-});
 
 export default function configureStore() {
 	const store = createStore(
