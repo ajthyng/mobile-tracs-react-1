4056d1fd28021480c038c9431514b81ae795a797
ajthyng <andrew.thyng@gmail.com>
2018-07-11 13:08:58 -0500

    Calendar screen now displays actual due dates, assignments, and has a marked up calendar

--- src/components/CalendarScreen/CalendarScreen.js
+++ src/components/CalendarScreen/CalendarScreen.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import {FlatList, Text} from 'react-native';
+import React, {Component} from 'react'
+import {FlatList, View, Text} from 'react-native'
 
-import {Calendar} from 'react-native-calendars';
-import { connect } from 'react-redux'
-import { setHeaderState } from '../../actions/header'
-import styled, {withTheme} from 'styled-components';
-import Header from '../CircleHeader/Header';
-import ItemSeparator from '../../_components/Notifications/ItemSeparator';
-import Ripple from 'react-native-material-ripple';
-import dayjs from 'dayjs';
+import {Calendar} from 'react-native-calendars'
+import {connect} from 'react-redux'
+import {setHeaderState} from '../../actions/header'
+import styled, {withTheme} from 'styled-components'
+import Header from '../CircleHeader/Header'
+import ItemSeparator from '../../_components/Notifications/ItemSeparator'
+import Ripple from 'react-native-material-ripple'
+import dayjs from 'dayjs'
 import PullHandle from './PullHandle'
 
 const ContainerView = styled.View`
@@ -16,18 +16,17 @@ const ContainerView = styled.View`
 	justify-content: center;
 	background-color: ${props => props.theme.viewBackground};
 	flex: 1;
-`;
+`
 
 const CalendarView = styled(Calendar)`
 	width: 100%;
-`;
+`
 
 const DueDatesList = styled(FlatList)`
-	flex: 1;
 	width: 100%;
-`;
+`
 
-const DueDateItem = styled(Ripple)`
+const DueDateContainer = styled(Ripple)`
 	width: 100%;
 	height: 50px;
 	background-color: ${props => props.theme.transparent};
@@ -36,7 +35,7 @@ const DueDateItem = styled(Ripple)`
 	padding-left: 16px;
 	padding-right: 16px;
 	flex-direction: row;
-`;
+`
 
 const SideColor = styled.View`
 	position: absolute;
@@ -44,18 +43,16 @@ const SideColor = styled.View`
 	width: 8px;
 	height: 50px;
 	background-color: ${props => props.color};
-`;
+`
 
 const ViewAllGrades = styled(Ripple)`
 	height: 40px;
 	background-color: white;
 	align-items: center;
 	justify-content: center;
-	position: absolute;
 	border-top-color: #36353480;
 	border-top-width: 1px;
-	left: 0;
-	right: 0;
+	width: 100%;
 	bottom: 0;
 `
 
@@ -65,8 +62,31 @@ const ViewAllGradesText = styled.Text`
 `
 
 const getRandomColor = (colors) => {
-	return colors[Math.floor(Math.random() * 100) % colors.length];
-};
+	return colors[Math.floor(Math.random() * 100) % colors.length]
+}
+
+class DueDateItem extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		const {item, color} = this.props
+		return (
+			<DueDateContainer>
+				<SideColor color={color} />
+				<View style={{flexDirection: 'column', width: 40, alignItems: 'center', justifyContent: 'center'}}>
+					<Text>{dayjs(item.dueDate).format('DD')}</Text>
+					<Text>{dayjs(item.dueDate).format('MMM')}</Text>
+				</View>
+				<View style={{flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'center'}}>
+					<Text style={{color: '#363534', fontSize: 14}}>{item.itemName}</Text>
+					<Text style={{color: '#36353480', fontSize: 12}}>{item.siteName}</Text>
+				</View>
+			</DueDateContainer>
+		)
+	}
+}
 
 class CalendarScreen extends Component {
 	static navigationOptions = {
@@ -74,51 +94,55 @@ class CalendarScreen extends Component {
 	}
 
 	constructor(props) {
-		super(props);
-		this.data = [
-			{key: '0', name: 'Assignment 1'},
-			{key: '1', name: 'Assignment 2'},
-			{key: '2', name: 'Assignment 3'},
-			{key: '3', name: 'Assignment 4'},
-			{key: '4', name: 'Assignment 5'},
-			{key: '5', name: 'Assignment 6'},
-			{key: '6', name: 'Assignment 7'},
-			{key: '7', name: 'Assignment 8'},
-			{key: '8', name: 'Assignment 9'},
-		];
-		this.colors = props.theme.assignments
+		super(props)
+		this.data = props.assignments
+		this.colorMapping = Object.keys(props.sites).reduce((accum, siteId) => {
+			accum[siteId] = getRandomColor(props.theme.assignments)
+			return accum
+		}, {})
+		console.log(this.colorMapping)
 		props.setHeaderState(true)
 	}
 
 	goToGradebook = () => {
-		this.props.navigation.navigate('Gradebook');
+		this.props.navigation.navigate('Gradebook')
+	}
+
+	renderItem = ({item}) => {
+		return <DueDateItem item={item} color={this.colorMapping[item.siteId]} />
 	}
 
 	render() {
+		const {assignments} = this.props
+
+		const markedDates = assignments.reduce((accum, assign) => {
+			const dueDate = dayjs(assign.dueDate)
+
+			accum[dueDate.format('YYYY-MM-DD')] = {selected: true, marked: false, selectedColor: this.colorMapping[assign.siteId]}
+			return accum
+		}, {})
+
 		return (
 			<ContainerView>
 				<CalendarView
 					current={new Date()}
 					minDate={'2018-05-01'}
 					monthFormat={'MMMM yyyy'}
+					markedDates={markedDates}
 				/>
 				<PullHandle />
 				<DueDatesList
 					data={this.data}
-					ItemSeparatorComponent={() => (<ItemSeparator/>)}
-					renderItem={({item}) => (
-						<DueDateItem key={item.key}>
-							<SideColor color={getRandomColor(this.colors)} />
-							<Text style={{textAlign: 'center'}}>{dayjs().format('DD\nMMM')}</Text>
-							<Text>{item.name}</Text>
-						</DueDateItem>
-					)}
+					ItemSeparatorComponent={() => (<ItemSeparator />)}
+					keyExtractor={(item, index) => index.toString(10)}
+					renderItem={this.renderItem}
+					bounces={false}
 				/>
 				<ViewAllGrades onPress={this.goToGradebook}>
 					<ViewAllGradesText>View All Grades</ViewAllGradesText>
 				</ViewAllGrades>
 			</ContainerView>
-		);
+		)
 	}
 }
 
@@ -130,10 +154,41 @@ const mapDispatchToProps = (dispatch, props) => ({
 	}
 })
 
+const byDueDate = (a, b) => {
+	const dueDateA = dayjs(a.dueDate)
+	const dueDateB = dayjs(b.dueDate)
+
+	let order = dueDateA.isBefore(dueDateB) ? -1 : 0
+	order = dueDateA.isAfter(dueDateB) ? 1 : order
+	return order
+}
+
 const mapStateToProps = state => {
-	console.log(state.grades)
+	let {grades: {grades}} = state
+	const gradesByDate = grades.reduce((accum, site) => {
+		const siteGrades = site.grades.map(grade => {
+			grade.siteName = site.name
+			grade.siteId = site.id
+			return grade
+		})
+		accum.push(...siteGrades)
+		return accum
+	}, []).filter(grade => {
+		const hasDueDate = !!grade.dueDate && grade.dueDate !== null && grade.dueDate !== undefined
+		let dueDateIsInFuture
+		if (hasDueDate) {
+			const dueDate = dayjs(grade.dueDate).startOf('day')
+			const today = dayjs().startOf('day')
+
+			dueDateIsInFuture = dueDate.isAfter(today) || dueDate.isSame(today)
+		}
+		const isNotGraded = grade.grade === null || grade.grade === undefined
+		return isNotGraded && dueDateIsInFuture
+	}).sort(byDueDate)
+
 	return {
-		grades: []
+		assignments: gradesByDate,
+		sites: state.tracsSites.userSites
 	}
 }
 
