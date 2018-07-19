7c49bb2fb362c3c80b0b079fd376d4722bde2ade
ajthyng <andrew.thyng@gmail.com>
2018-07-19 11:51:26 -0500

    GradeComment does not need to inherit from Component

--- src/components/CourseScreen/GradeComment.js
+++ src/components/CourseScreen/GradeComment.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {TouchableOpacity} from "react-native"
 import styled from 'styled-components'
