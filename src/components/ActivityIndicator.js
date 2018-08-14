25df6131a64c8cc3af99d6b5bcd31674d9129f49
ajthyng <andrew.thyng@gmail.com>
2018-08-14 11:48:29 -0500

    Changed image on activity indiactor, still will never rotate on center

--- src/components/ActivityIndicator.js
+++ src/components/ActivityIndicator.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import {Animated, Easing, Image, StyleSheet, View} from 'react-native'
 import styled, {withTheme} from 'styled-components'
-const tracsLogo = require('../../img/tracs.png')
+
+const tracsLogo = require('../../img/tracs-logo-center.png')
 
 const Container = styled.View`
 	flex: 1;
@@ -13,25 +14,22 @@ const Container = styled.View`
 
 const ImageContainer = styled(Animated.View)`
 	width: ${props => props.size}px;
-	height: ${props => props.size * 0.956284153}px;
+	height: ${props => props.size}px;
 	align-items: center;
 	justify-content: flex-end;
 `
 
 const TracsLOGO = styled.Image`
-	width: ${props => props.size * props.aspectRatio}px;
+	width: ${props => props.size}px;
 	height: ${props => props.size}px;
 `
 
 class ActivityIndicator extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			animationRange: new Animated.Value(0)
-		}
-	}
+  state = {
+    animationRange: new Animated.Value(0)
+  }
 
-	rotateImage = () => {
+  rotateImage = () => {
     Animated.loop(
       Animated.timing(this.state.animationRange, {
         toValue: 1,
@@ -40,42 +38,40 @@ class ActivityIndicator extends Component {
         useNativeDriver: true
       })
     ).start()
-	}
+  }
 
-	componentDidMount() {
-		this.rotateImage()
-	}
+  componentDidMount() {
+    this.rotateImage()
+  }
 
-	render() {
-		const {size, aspectRatio} = this.props
-		const {animationRange} = this.state
+  render() {
+    const {size} = this.props
+    const {animationRange} = this.state
 
-		const rotate = animationRange.interpolate({
-			inputRange: [0, 1],
-			outputRange: ['0deg', '360deg']
-		})
+    const rotate = animationRange.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
 
-		const transform = {
-			transform: [{rotate}]
-		}
+    const transform = {
+      transform: [{rotate}]
+    }
 
-		return (
-			<Container>
-				<ImageContainer style={transform} size={size}>
-					<TracsLOGO
-            aspectRatio={aspectRatio}
-						size={size}
-						source={tracsLogo}
-					/>
-				</ImageContainer>
-			</Container>
-		)
-	}
+    return (
+      <Container>
+        <ImageContainer style={transform} size={size}>
+          <TracsLOGO
+            size={size}
+            source={tracsLogo}
+          />
+        </ImageContainer>
+      </Container>
+    )
+  }
 }
 
 ActivityIndicator.defaultProps = {
-	size: 80,
-	aspectRatio: (1438 / 1378)
+  size: 80
 }
 
 export default withTheme(ActivityIndicator)
