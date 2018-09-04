28c3c4990261b1dbb60054f6e65b51d48fc14dcc
ajthyng <andrew.thyng@gmail.com>
2018-09-04 12:30:27 -0500

    Added maria's arrow

--- src/components/CourseList/EmptyCourseList.js
+++ src/components/CourseList/EmptyCourseList.js
@@ -10,11 +10,6 @@ const Container = styled.View`
   justify-content: flex-start;
 `
 
-const Arrow = styled.View`
-  width: 100%;
-  height: 40px;
-`
-
 const EmptyMessage = styled.Text`
   text-align: center;
   color: #808080;
@@ -49,7 +44,8 @@ class EmptyCourseList extends PureComponent {
     return (
       <Container style={this.state}>
         <Svg height='55' width='50' >
-          <Path strokeWidth={2} d='M 40 5 C 40 25 25 51 2 51 M 40.5 5 L 45 20 M 40.5 5 L 30 18' fill='none' stroke='#808080' />
+          <Path d='M29.73,24.23q3.39-4.82,6.5-9.82l-.91-.12a30.62,30.62,0,0,0,4.24,9c.36.53,1.23,0,.87-.5A29.94,29.94,0,0,1,36.28,14a.5.5,0,0,0-.91-.12q-3.12,5-6.5,9.82c-.37.53.5,1,.86.5Z' fill='none' stroke='#808080' />
+          <Path d='M24.69,44.18A20.27,20.27,0,0,0,35.14,20.1a.5.5,0,0,0-1,.26,19.27,19.27,0,0,1-10,23c-.58.28-.07,1.15.51.86Z' fill='none' stroke='#808080' />
         </Svg>
         <EmptyMessage>
           {message}
