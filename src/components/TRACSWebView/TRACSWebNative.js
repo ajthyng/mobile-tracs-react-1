efd10d4af6de4111315052b9c3ccd36cbc5e544b
ajthyng <andrew.thyng@gmail.com>
2018-09-17 08:53:45 -0500

    Fixed broken android release build due to RN upgrade

--- src/components/TRACSWebView/TRACSWebNative.js
+++ src/components/TRACSWebView/TRACSWebNative.js
@@ -1,17 +1,9 @@
-/**
- * Copyright 2018 Andrew Thyng
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 import React, {Component} from 'react'
 import {BackHandler, Dimensions, WebView, Platform, StyleSheet, requireNativeComponent} from 'react-native'
 import WebError from './WebError'
 import {Analytics} from '../../utils/analytics'
 import {withNavigation} from 'react-navigation'
+import ActivityIndicator from '../ActivityIndicator'
 
 const TRACSWeb = requireNativeComponent('TRACSWeb', TRACSWebView)
 const styles = StyleSheet.create({
@@ -75,6 +67,7 @@ class TRACSWebView extends Component {
           style={styles.webView}
           baseUrl={url}
           injectedJavaScript={removeHeaderJS}
+          renderLoading={ActivityIndicator}
         />
       )
     })
