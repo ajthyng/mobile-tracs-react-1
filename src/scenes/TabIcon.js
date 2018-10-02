613ff4db4e6c86586ab05950f8b41ebd67f51815
ajthyng <andrew.thyng@gmail.com>
2018-10-02 15:51:27 -0500

    Added badge indicator to tab icons
    The icon will look for the course card in navigation parameters, and
    it will use that along with the data in the redux store and its own
    icon name in order to determine whether a dot should be displayed.

--- src/scenes/TabIcon.js
+++ src/scenes/TabIcon.js
@@ -1,6 +1,20 @@
 import React, { PureComponent } from 'react'
 import FA5ProIcon from 'react-native-vector-icons/FontAwesome5Pro'
 import styled from 'styled-components'
+import { withNavigation } from 'react-navigation'
+import { connect } from 'react-redux'
+
+const Container = styled.View``
+
+const Dot = styled.View`
+  height: 8px;
+  width: 8px;
+  border-radius: 4px;
+  position: absolute;
+  top: 3;
+  right: -3;
+  background-color: #b30e1b;
+`
 
 const Icon = styled(FA5ProIcon)`
   color: ${props => props.tintColor};
@@ -9,13 +23,38 @@ const Icon = styled(FA5ProIcon)`
 `
 
 class TabIcon extends PureComponent {
+  renderDot = () => {
+    const { iconName, newAnnouncements, newForumPosts } = this.props
+
+    switch (iconName) {
+      case 'comment':
+        return newForumPosts ? <Dot /> : null
+      case 'bullhorn':
+        return newAnnouncements ? <Dot /> : null
+      default:
+        return null
+    }
+  }
+
   render () {
     const { focused, iconName, tintColor } = this.props
 
     return (
-      <Icon name={iconName} solid={focused} tintColor={tintColor} />
+      <Container>
+        <Icon name={iconName} solid={focused} tintColor={tintColor} />
+        {this.renderDot()}
+      </Container>
     )
   }
 }
 
-export default TabIcon
+const mapStateToProps = (state, props) => {
+  const course = props.navigation && props.navigation.getParam('course', null)
+  const { id = '' } = course
+  return {
+    newAnnouncements: (state.notifications.badgeCounts[id] || {}).announceCount > 0,
+    newForumPosts: (state.notifications.badgeCounts[id] || {}).forumCount > 0
+  }
+}
+
+export default withNavigation(connect(mapStateToProps, null)(TabIcon))
